172144ca25a9af4d29ad01f8f1ddfb7c18482ec8
Filippo Albertini <filippo@filippoalbertini.it>
2022-03-09 09:24:33 +0100

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_Prof_Pizzeria/frmMain.cs
@@ -0,0 +1,10 @@
+namespace AS2122_4E_INF_Prof_Pizzeria
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
